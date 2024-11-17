8903ce30f6835b9886a7bcde15ecfabaaa37aa5f
Cristhian <ccdelgadop@gmail.com>
2024-11-17 17:14:17 -0500

    add ocommentary

--- src/db.py
+++ src/db.py
@@ -6,6 +6,7 @@ DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_NAME = os.getenv('DB_NAME')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
+# add comentary
 
 if DB_USER:
     DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
